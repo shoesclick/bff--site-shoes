d375e863affb6bdb00296340c48caf12a18c9bf0
git <claytnmorais@gmail.com>
2024-03-11 20:21:12 -0300

    Refactory Stack

--- src/main/java/com/shoesclick/bff/site/shoes/Application.java
+++ src/main/java/com/shoesclick/bff/site/shoes/Application.java
@@ -2,8 +2,10 @@ package com.shoesclick.bff.site.shoes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableCaching
 @SpringBootApplication
 @EnableFeignClients
 public class Application {
---
+++ src/main/java/com/shoesclick/bff/site/shoes/config/CacheConfig.java
@@ -0,0 +1,61 @@
+package com.shoesclick.bff.site.shoes.config;
+
+import com.shoesclick.bff.site.shoes.config.properties.CacheProperties;
+import org.springframework.cache.CacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
+import java.util.Objects;
+
+
+@Configuration
+public class CacheConfig {
+
+    private final CacheProperties cacheProperties;
+
+
+    public CacheConfig(CacheProperties cacheProperties) {
+        this.cacheProperties = cacheProperties;
+    }
+
+
+    @Bean
+    public JedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration redisStandalone = new RedisStandaloneConfiguration(cacheProperties.host(), cacheProperties.port());
+        JedisConnectionFactory factory = new JedisConnectionFactory(redisStandalone);
+        factory.getPoolConfig().setMinIdle(10);
+        factory.getPoolConfig().setMaxIdle(30);
+        return factory;
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory){
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        return redisTemplate;
+    }
+
+    @Bean
+    @Primary
+   public CacheManager cacheManager(){
+        return RedisCacheManager.builder()
+                .fromConnectionFactory(redisConnectionFactory())
+                .cacheDefaults(
+                        RedisCacheConfiguration.defaultCacheConfig().entryTtl(
+                                Duration.ofHours(cacheProperties.ttlCache())
+                        )
+                ).build();
+    }
+
+
+}
